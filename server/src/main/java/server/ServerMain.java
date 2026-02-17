190e068d62d43798aefbaa75a5d902d54af8f485
brandosha <brandon.lantz@icloud.com>
2026-02-17 15:09:36 -0700

    Phase 3: Getting started

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -1,10 +1,12 @@
 package server;
 
-import chess.*;
-
 public class ServerMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        int port = 8080;
+
+        Server server = new Server();
+        server.run(port);
+
+        System.out.println("♕ 240 Chess Server running on port " + port);
     }
 }
