12bf3e21d794958c3ce294bbf19bcbb875430c08
brandosha <brandon.lantz@icloud.com>
2026-02-17 15:31:01 -0700

    Phase 3: Create data model classes

---
+++ shared/src/main/java/datamodel/GameData.java
@@ -0,0 +1,19 @@
+package datamodel;
+
+import chess.ChessGame;
+
+public class GameData {
+  public int gameID;
+  public String whiteUsername;
+  public String blackUsername;
+  public String gameName;
+  public ChessGame game;
+
+  public GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    this.gameID = gameID;
+    this.whiteUsername = whiteUsername;
+    this.blackUsername = blackUsername;
+    this.gameName = gameName;
+    this.game = game;
+  }
+}
---
+++ shared/src/main/java/datamodel/UserData.java
@@ -0,0 +1,13 @@
+package datamodel;
+
+public class UserData {
+  public String username;
+  public String password;
+  public String email;
+
+  public UserData(String username, String password, String email) {
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+}
