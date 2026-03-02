1c8694669dff47f5ecbe69dcdc823d42126f14e5
brandosha <brandon.lantz@icloud.com>
2026-03-02 16:40:16 -0700

    Phase 3: Write tests for register and login

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,33 +1,62 @@
 package service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import dataaccess.Database;
 import dataaccess.DatabaseMemory;
+import datamodel.http.LoginRequest;
 import datamodel.http.RegisterRequest;
 
 public class UserServiceTests {
 
   UserService service;
   Database db;
+  RegisterRequest defaultUser = new RegisterRequest("username", "password", "email");
 
   @BeforeEach
-  void beforeEach() {
+  public void beforeEach() {
     db = new DatabaseMemory();
     service = new UserService(db);
   }
   
   @Test
   public void registerPositive() throws Exception {
-    var req = new RegisterRequest("username", "password", "email");
+    var req = this.defaultUser;
     var res = service.register(req);
 
-    assert res.username.equals(req.username);
+    var user = db.userDao().getUser(req.username);
+    assertEquals(user.username, req.username);
+    assertEquals(user.email, req.email);
+    assertEquals(user.password, req.password);
+
+    var auth = db.userDao().getAuth(res.authToken);
+    assertEquals(auth.username, req.username);
   }
 
   @Test
   public void registerNegative() throws Exception {
-    
+    service.register(defaultUser);
+
+    assertThrows(AlreadyTakenException.class, () -> service.register(defaultUser));
+  }
+
+  @Test
+  public void loginPositive() throws Exception {
+    service.register(defaultUser);
+
+    var res = service.login(new LoginRequest(defaultUser.username, defaultUser.password));
+    var auth = db.userDao().getAuth(res.authToken);
+
+    assertEquals(auth.username, defaultUser.username);
+  }
+
+  @Test
+  public void loginNegative() throws Exception {
+    var loginReq = new LoginRequest("no-exist", "");
+
+    assertThrows(UnauthorizedException.class, () -> service.login(loginReq));
   }
 }
