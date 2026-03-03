b688d0027e1660533156424b96c5bbf3794ffd48
brandosha <brandon.lantz@icloud.com>
2026-03-02 17:11:58 -0700

    Phase 3: Write tests for `GameService` methods

---
+++ server/src/test/java/service/GameServiceTests.java
@@ -0,0 +1,96 @@
+package service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dataaccess.Database;
+import dataaccess.DatabaseMemory;
+import datamodel.UserData;
+import datamodel.http.CreateGameRequest;
+import datamodel.http.JoinGameRequest;
+import datamodel.http.RegisterRequest;
+
+public class GameServiceTests {
+  GameService service;
+  Database db;
+
+  UserData defaultUser1 = new UserData("username1", "password", "email");
+  String authToken1;
+
+  UserData defaultUser2 = new UserData("username2", "password", "email");
+  String authToken2;
+  
+
+  @BeforeEach
+  public void beforeEach() throws Exception {
+    db = new DatabaseMemory();
+    service = new GameService(db);
+
+    UserService userService = new UserService(db);
+    var res1 = userService.register(
+      new RegisterRequest(defaultUser1.username, defaultUser1.password, defaultUser1.email)
+    );
+    authToken1 = res1.authToken;
+
+    var res2 = userService.register(
+      new RegisterRequest(defaultUser2.username, defaultUser2.password, defaultUser2.email)
+    );
+    authToken2 = res2.authToken;
+  }
+
+  @Test
+  public void createGamePositive() throws Exception {
+    var req = new CreateGameRequest("new game");
+    var res = service.createGame(req, authToken1);
+
+    var game = db.gameDao().getGame(res.gameID);
+    assertEquals(req.gameName, game.gameName);
+  }
+
+  @Test
+  public void createGameNegative() throws Exception {
+    var req = new CreateGameRequest("new game");
+    assertThrows(UnauthorizedException.class, () -> service.createGame(req, "fake-auth-token"));
+  }
+
+  @Test
+  public void listGamesPositive() throws Exception {
+    var req = new CreateGameRequest("new game");
+    var createRes = service.createGame(req, authToken1);
+    var game = db.gameDao().getGame(createRes.gameID);
+
+    var list = service.listGames(authToken1);
+    assertEquals(1, list.games.size());
+    assertEquals(game, list.games.iterator().next());
+  }
+
+  @Test
+  public void listGamesNegative() throws Exception {
+    assertThrows(UnauthorizedException.class, () -> service.listGames("fake-auth-token"));
+  }
+
+  @Test
+  public void joinGamePositive() throws Exception {
+    var createReq = new CreateGameRequest("new game");
+    var createRes = service.createGame(createReq, authToken1);
+
+    var req = new JoinGameRequest("BLACK", createRes.gameID);
+    service.joinGame(req, authToken1);
+
+    var game = db.gameDao().getGame(createRes.gameID);
+    assertEquals(defaultUser1.username, game.blackUsername);
+  }
+
+  @Test
+  public void joinGameNegative() throws Exception {
+    var createReq = new CreateGameRequest("new game");
+    var createRes = service.createGame(createReq, authToken1);
+
+    var req = new JoinGameRequest("BLACK", createRes.gameID);
+    service.joinGame(req, authToken1);
+    
+    assertThrows(AlreadyTakenException.class, () -> service.joinGame(req, authToken2));
+  }
+}
--- shared/src/main/java/datamodel/GameData.java
+++ shared/src/main/java/datamodel/GameData.java
@@ -16,4 +16,51 @@ public class GameData {
     this.gameName = gameName;
     this.game = game;
   }
+  
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + gameID;
+    result = prime * result + ((whiteUsername == null) ? 0 : whiteUsername.hashCode());
+    result = prime * result + ((blackUsername == null) ? 0 : blackUsername.hashCode());
+    result = prime * result + ((gameName == null) ? 0 : gameName.hashCode());
+    result = prime * result + ((game == null) ? 0 : game.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    GameData other = (GameData) obj;
+    if (gameID != other.gameID)
+      return false;
+    if (whiteUsername == null) {
+      if (other.whiteUsername != null)
+        return false;
+    } else if (!whiteUsername.equals(other.whiteUsername))
+      return false;
+    if (blackUsername == null) {
+      if (other.blackUsername != null)
+        return false;
+    } else if (!blackUsername.equals(other.blackUsername))
+      return false;
+    if (gameName == null) {
+      if (other.gameName != null)
+        return false;
+    } else if (!gameName.equals(other.gameName))
+      return false;
+    if (game == null) {
+      if (other.game != null)
+        return false;
+    } else if (!game.equals(other.game))
+      return false;
+    return true;
+  }
 }
