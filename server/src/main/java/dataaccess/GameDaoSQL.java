a00fed0760cb80cc4be93b18af1b33bfff54a3bf
brandosha <brandon.lantz@icloud.com>
2026-03-07 17:41:33 -0700

    Phase 4: Implement remaining `GameDaoSQL` methods

--- server/src/main/java/dataaccess/GameDaoSQL.java
+++ server/src/main/java/dataaccess/GameDaoSQL.java
@@ -1,18 +1,27 @@
 package dataaccess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
+import com.google.gson.Gson;
+
+import chess.ChessGame;
 import datamodel.GameData;
 
 public class GameDaoSQL implements GameDao {
   private final String tableName = "games";
+
+  Gson gson = new Gson();
 
   public GameDaoSQL() {
     DatabaseManager.forceCreateDatabase();
 
     var createTable = "CREATE TABLE IF NOT EXISTS " + tableName + " ("
       + "  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,"
-      + "  game VARCHAR(512)"
+      + "  name VARCHAR(32) NOT NULL,"
+      + "  whiteUsername VARCHAR(32),"
+      + "  blackUsername VARCHAR(32),"
+      + "  gameState VARCHAR(512) NOT NULL"
       + ")";
     try (var conn = DatabaseManager.getConnection()) {
       try (var statement = conn.createStatement()) {
@@ -39,19 +48,77 @@ public class GameDaoSQL implements GameDao {
 
   @Override
   public void insertGame(GameData game) {
-    // TODO Auto-generated method stub
+    var insert = "INSERT INTO " + tableName
+      + "  (name, whiteUsername, blackUsername, gameState)"
+      + "  VALUES (?, ?, ?, ?);";
+
+    try (var conn = DatabaseManager.getConnection()) {
+      try (var statement = conn.prepareStatement(insert)) {
+        statement.setString(1, game.gameName);
+        statement.setString(2, game.whiteUsername);
+        statement.setString(3, game.blackUsername);
+        statement.setString(4, gson.toJson(game.game));
+        statement.executeUpdate();
+      }
+    } catch (Exception e) {
+      // TODO: Proper error handling
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
   public GameData getGame(int id) {
-    // TODO Auto-generated method stub
+    var select = "SELECT id, name, whiteUsername, blackUsername, gameState FROM " + tableName
+      + "  WHERE id = ?";
+
+    try (var conn = DatabaseManager.getConnection()) {
+      try (var statement = conn.prepareStatement(select)) {
+        statement.setInt(1, id);
+        var results = statement.executeQuery();
+        if (results.next()) {
+          var gameJson = results.getString(5);
+          return new GameData(
+            results.getInt(1),
+            results.getString(2),
+            results.getString(3),
+            results.getString(4),
+            gson.fromJson(gameJson, ChessGame.class)
+          );
+        }
+      }
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+
     return null;
   }
 
   @Override
   public Collection<GameData> listGames() {
-    // TODO Auto-generated method stub
-    return null;
+    var select = "SELECT id, name, whiteUsername, blackUsername, gameState FROM " + tableName;
+
+    var list = new ArrayList<GameData>();
+    try (var conn = DatabaseManager.getConnection()) {
+      try (var statement = conn.createStatement()) {
+        var results = statement.executeQuery(select);
+        while (results.next()) {
+          var gameJson = results.getString(5);
+          var data = new GameData(
+            results.getInt(1),
+            results.getString(2),
+            results.getString(3),
+            results.getString(4),
+            gson.fromJson(gameJson, ChessGame.class)
+          );
+
+          list.add(data);
+        }
+      }
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+
+    return list;
   }
 
   @Override
